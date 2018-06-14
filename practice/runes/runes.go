e56b1457d0a7ec06519ddeba3526bb29da98b224
Luciano Ramalho <luciano@ramalho.org>
2018-06-14 14:57:04 -0300

    match with table tests

--- practice/runes/runes.go
+++ practice/runes/runes.go
@@ -3,10 +3,23 @@ package main
 import (
 	"strings"
 	"strconv"
+	"io"
+	"github.com/standupdev/strset"
 )
 
 func ParseEntry(line string) (rune, string) {
 	fields := strings.Split(line, ";")
 	code, _ := strconv.ParseInt(fields[0], 16, 32)
 	return rune(code), fields[1]
+}
+
+func match(query, name string) bool {
+	normalized := strings.Replace(strings.ToUpper(query), "-", " ", -1)
+	queryTerms := strset.MakeFromText(normalized)
+	nameTerms := strset.MakeFromText(strings.Replace(name, "-", " ", -1))
+	return queryTerms.SubsetOf(nameTerms)
+}
+
+func Filter(dbFile io.Reader, query string) []rune {
+	return []rune{'A'}
 }
--- practice/runes/runes_test.go
+++ practice/runes/runes_test.go
@@ -1,9 +1,13 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"strings"
+)
+
+const capitalA = `0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;`
 
 func TestParseEntry(t *testing.T) {
-	capitalA := `0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;`
 	char, name := ParseEntry(capitalA)
 	want := 'A'
 	if char != want {
@@ -15,3 +19,48 @@ func TestParseEntry(t *testing.T) {
 	}
 }
 
+func TestFilter(t *testing.T) {
+	query := "LETTER A"
+	dbFile := strings.NewReader(capitalA)
+	results := Filter(dbFile, query)
+	if len(results) != 1 {
+		t.Errorf("Got %d results, expected 1", len(results))
+	}
+}
+
+func TestMatch(t *testing.T) {
+	query := "LETTER A"
+	name := "LATIN CAPITAL LETTER A"
+	want := true
+	got := match(query, name)
+	if got != want {
+		t.Errorf("Match(%q, %q) got: %v, want: %v", query, name, got, want)
+	}
+}
+
+func TestMatch_table(t *testing.T) {
+	var testCases = []struct {
+		query  	string
+		name   	string
+		want 	bool
+	}{
+		{"A", "A", true},
+		{"A", "A B", true},
+		{"A", "AB", false},
+		{"A B", "A B", true},
+		{"A B", "A B C", true},
+		{"C A", "A B C", true},
+		{"c A", "A B C", true},
+		{"A B", "A B-C", true},
+		{"A-B", "A B C", true},
+	}
+	for _, tc := range testCases {
+		testName := tc.query + "⊆" + tc.name
+		t.Run(testName, func(t *testing.T) {
+			got := match(tc.query, tc.name)
+			if tc.want != got {
+				t.Errorf("got: %#v, want: %#v", got, tc.want)
+			}
+		})
+	}
+}
