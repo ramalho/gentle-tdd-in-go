e91dbb91916d47f85e5ee1861004503be6a132a7
Luciano Ramalho <luciano@ramalho.org>
2018-07-05 22:54:36 -0300

    end state of first dojo at ThoughtWorks

--- examples/dojorunes/runes.go
+++ examples/dojorunes/runes.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"strconv"
+	"bufio"
+	"os"
+	"io"
+)
+
+func ParseLine(line string) (rune, string) {
+	fields := strings.Split(line, ";")
+	code, _ := strconv.ParseInt(fields[0], 16, 32)
+	return rune(code), fields[1]
+}
+
+func FormatLine(char rune, name string) string {
+	return fmt.Sprintf("U+%04X\t%c\t%v", char, char, name)
+}
+
+func Search(data io.Reader, query string) string {
+	scanner := bufio.NewScanner(data)
+	builder := strings.Builder{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.TrimSpace(line) == "" {
+			continue
+
+		}
+		if strings.Contains(line, strings.ToUpper(query)) {
+			builder.WriteString(FormatLine(ParseLine(line)))
+			builder.WriteString("\n")
+		}
+	}
+	return builder.String()
+}
 
 func main() {
-	fmt.Println("Please provide one or more words to search.")
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide one or more words to search.")
+		return
+	}
+
+	data, err := os.Open("UnicodeData.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	result := Search(data, strings.Join(os.Args[1:], " "))
+
+	fmt.Print(result)
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
--- examples/dojorunes/runes_test.go
+++ examples/dojorunes/runes_test.go
@@ -1,6 +1,89 @@
 package main
 
+import (
+	"testing"
+	"strings"
+)
+
 func Example() {
 	main()
-	// Output: Please provide one or more words to search.
+	// Output:
+	// Please provide one or more words to search.
 }
+
+const A = `0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;`
+
+func TestParseLine(t *testing.T) {
+	char, name := ParseLine(A)
+	wantChar := 'A'
+	if char != wantChar {
+		t.Errorf("Got: %q, want: %q", char, wantChar)
+	}
+	wantName := "LATIN CAPITAL LETTER A"
+	if name != wantName {
+		t.Errorf("Got: %q, want: %q", name, wantName)
+	}
+}
+
+func TestFormatLine(t *testing.T) {
+	got := FormatLine(ParseLine(A))
+	want := "U+0041\tA\tLATIN CAPITAL LETTER A"
+	if got != want {
+		t.Errorf("Got: %q, want: %q", got, want)
+	}
+}
+
+const Cruzeiro = "20A2;CRUZEIRO SIGN;Sc;0;ET;;;;;N;;;;;"
+
+func TestFormatLine_HexadecimalWithLetter(t *testing.T) {
+	got := FormatLine(ParseLine(Cruzeiro))
+	want := "U+20A2\t₢\tCRUZEIRO SIGN"
+	if got != want {
+		t.Errorf("Got: %q, want: %q", got, want)
+	}
+
+}
+
+const Data = `20A0;EURO-CURRENCY SIGN;Sc;0;ET;;;;;N;;;;;
+20A1;COLON SIGN;Sc;0;ET;;;;;N;;;;;
+20A2;CRUZEIRO SIGN;Sc;0;ET;;;;;N;;;;;`
+
+func TestSearch(t *testing.T) {
+	query := "CRUZEIRO"
+	data := strings.NewReader(Data)
+	got := Search(data,query)
+	want := "U+20A2\t₢\tCRUZEIRO SIGN\n"
+	if got != want {
+		t.Errorf("Got: %q, want: %q", got, want)
+	}
+}
+
+func TestSearch_WithThreeResults(t *testing.T) {
+	query := "SIGN"
+	data := strings.NewReader(Data)
+	got := Search(data,query)
+	want := "U+20A0\t₠\tEURO-CURRENCY SIGN\nU+20A1\t₡\tCOLON SIGN\nU+20A2\t₢\tCRUZEIRO SIGN\n"
+	if got != want {
+		t.Errorf("Got: %q, want: %q", got, want)
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
