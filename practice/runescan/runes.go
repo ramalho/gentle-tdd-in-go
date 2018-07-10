937ce9f59a190526bcb019ec418e266219832ba2
Luciano Ramalho <luciano@ramalho.org>
2018-07-10 20:54:47 -0300

    runescan: first test of ParseLine

--- practice/runescan/runes.go
+++ practice/runescan/runes.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Please provide one or more words to search.")
+}
+
+// ParseLine extracts fields from a line in UnicodeData.txt
+func ParseLine(line string) string {
+	fields := strings.Split(line, ";")
+	return fields[1]
 }
--- practice/runescan/runes_test.go
+++ practice/runescan/runes_test.go
@@ -1,7 +1,18 @@
 package main
 
+import "testing"
+
 func Example() {
 	main()
 	// Output:
 	// Please provide one or more words to search.
+}
+
+func TestParseLine(t *testing.T) {
+	line := "005A;LATIN CAPITAL LETTER Z;Lu;0;L;;;;;N;;;;007A;"
+	got := ParseLine(line)
+	want := "LATIN CAPITAL LETTER Z"
+	if got != want {
+		t.Errorf("Got: %q, want: %q", got, want)
+	}
 }
