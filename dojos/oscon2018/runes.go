72c0d049b21331ec9ef4a1ec2ee271ccbdb6f452
Luciano Ramalho <luciano@ramalho.org>
2018-08-08 19:29:15 -0300

    slides in Portuguese for Women Who Go Workshop in São Paulo

--- dojos/oscon2018/runes.go
+++ dojos/oscon2018/runes.go
@@ -1,1 +1,71 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide one or more words to search.")
+		return
+	}
+	query := strings.Join(os.Args[1:], " ")
+	data, err := os.Open("UnicodeData.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer func() { data.Close() }()
+
+	for _, result := range Select(data, query) {
+		char := StringToRune(result.Code)
+		fmt.Printf("U+%v\t%c\t%v\n", result.Code, char, result.Name)
+	}
+}
+
+func Parse(line string) (string, string) {
+	a := strings.Split(line, ";")
+	return a[0], a[1]
+}
+
+func Match(query string, name string) bool {
+	if query == "" {
+		return false
+	}
+	queryTerms := strings.Split(strings.ToUpper(query), " ")
+	for _, queryTerm := range queryTerms {
+		if !strings.Contains(name, queryTerm) {
+			return false
+		}
+	}
+	return true
+}
+
+type CodeName struct {
+	Code string
+	Name string
+}
+
+func Select(data io.Reader, query string) []CodeName {
+	scanner := bufio.NewScanner(data)
+	result := []CodeName{}
+	for scanner.Scan() {
+		code, name := Parse(scanner.Text())
+		if Match(query, name) {
+			result = append(result, CodeName{code, name})
+		}
+	}
+	return result
+}
+
+func StringToRune(strHex string) rune {
+	code, err := strconv.ParseInt(strHex, 16, 32)
+	if err != nil {
+		panic(err)
+	}
+	return rune(code)
+}
--- dojos/oscon2018/runes_test.go
+++ dojos/oscon2018/runes_test.go
@@ -1,1 +1,107 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Example() {
+	main()
+	// Output:
+	// Please provide one or more words to search.
+}
+func Example_chess_black() {
+	savedArgs := os.Args
+	defer func() { os.Args = savedArgs }()
+	os.Args = []string{"", "chess black"}
+	main()
+	// Output:
+	// U+265A	♚	BLACK CHESS KING
+	// U+265B	♛	BLACK CHESS QUEEN
+	// U+265C	♜	BLACK CHESS ROOK
+	// U+265D	♝	BLACK CHESS BISHOP
+	// U+265E	♞	BLACK CHESS KNIGHT
+	// U+265F	♟	BLACK CHESS PAWN
+}
+
+func TestParse(t *testing.T) {
+	// Given
+	const line = "0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;"
+
+	// When
+	gotCode, gotName := Parse(line)
+
+	// Then
+	wantCode := "0041"
+	if wantCode != gotCode {
+		t.Errorf("code: %v, want: %v", gotCode, wantCode)
+	}
+	wantName := "LATIN CAPITAL LETTER A"
+	if wantName != gotName {
+		t.Errorf("name: %v, want: %v", gotName, wantName)
+	}
+}
+
+func TestMatch(t *testing.T) {
+	testCases := []struct {
+		query string
+		name  string
+		want  bool
+	}{
+		{"BLACK", "BLACK CHESS KING", true},
+		{"WHITE", "BLACK CHESS KING", false},
+		{"black", "BLACK CHESS KING", true},
+		{"chess black", "BLACK CHESS KING", true},
+		{"", "BLACK CHESS KING", false},
+		{"BLACK CAT", "BLACK CHESS KING", false},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%v in %v", tc.query, tc.name),
+			func(t *testing.T) {
+				// Given
+				query := tc.query
+				name := tc.name
+
+				// When
+				got := Match(query, name)
+				if got != tc.want {
+					t.Errorf("got %v; want %v", got, tc.want)
+				}
+			})
+	}
+}
+
+const sample = `003D;EQUALS SIGN;Sm;0;ON;;;;;N;;;;;
+003E;GREATER-THAN SIGN;Sm;0;ON;;;;;Y;;;;;
+003F;QUESTION MARK;Po;0;ON;;;;;N;;;;;
+0040;COMMERCIAL AT;Po;0;ON;;;;;N;;;;;
+0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;
+0042;LATIN CAPITAL LETTER B;Lu;0;L;;;;;N;;;;0062;
+`
+
+func TestSelect(t *testing.T) {
+	// Given
+	query := "LETTER"
+	data := strings.NewReader(sample)
+	// When
+	got := Select(data, query)
+	// Then
+	want := []CodeName{
+		{"0041", "LATIN CAPITAL LETTER A"},
+		{"0042", "LATIN CAPITAL LETTER B"},
+	}
+	assert.Equal(t, want, got)
+}
+
+func TestStringToRune(t *testing.T) {
+	// GIVEN
+	code := "0041"
+	// When
+	got := StringToRune(code)
+	// Then
+	assert.Equal(t, 'A', got)
+}
